699526f73ca9ded4602ed7351d49d45d56bf1777
Dylan Sherry <sharshofski@gmail.com>
2015-03-26 22:18:59 -0400

    First commit

--- Source/content_script.js
+++ Source/content_script.js
@@ -1,42 +1,37 @@
 walk(document.body);
 
-function walk(node) 
+function walk(node)
 {
-	// I stole this function from here:
-	// http://is.gd/mwZp7E
-	
-	var child, next;
+    // I stole this function from here:
+    // http://is.gd/mwZp7E
 
-	switch ( node.nodeType )  
+    var child, next;
+
+    switch ( node.nodeType )
+    {
+    case 1:  // Element
+    case 9:  // Document
+    case 11: // Document fragment
+	child = node.firstChild;
+	while ( child )
 	{
-		case 1:  // Element
-		case 9:  // Document
-		case 11: // Document fragment
-			child = node.firstChild;
-			while ( child ) 
-			{
-				next = child.nextSibling;
-				walk(child);
-				child = next;
-			}
-			break;
-
-		case 3: // Text node
-			handleText(node);
-			break;
+	    next = child.nextSibling;
+	    walk(child);
+	    child = next;
 	}
+	break;
+
+    case 3: // Text node
+	handleText(node);
+	break;
+    }
 }
 
-function handleText(textNode) 
+function handleText(textNode)
 {
-	var v = textNode.nodeValue;
-
-	v = v.replace(/\bThe Cloud\b/g, "My Butt");
-	v = v.replace(/\bThe cloud\b/g, "My butt");
-	v = v.replace(/\bthe Cloud\b/g, "my Butt");
-	v = v.replace(/\bthe cloud\b/g, "my butt");
-	
-	textNode.nodeValue = v;
+    var v = textNode.nodeValue;
+    v = v.replace(/[aeiou]/gi, "");
+    textNode.nodeValue = v;
 }
 
 
